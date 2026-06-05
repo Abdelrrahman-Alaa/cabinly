a85b88531bad87267165ef5dc9daf03f59f30daf
Abdelrahman Alaa <Abdelrrahman.Alaa@gmail.com>
2026-06-05 07:59:35 +0300

    feat: Integrate Supabase for data management and add React Query for state management

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,8 +1,18 @@
 import { useEffect } from "react";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
+import { getCabins } from "../services/apiCabins";
 
 function Cabins() {
+  useEffect(() => {
+    async function fetchCabins() {
+      const cabins = await getCabins();
+      console.log(cabins);
+    }
+
+    fetchCabins();
+  }, []);
+
   return (
     <Row type="horizontal">
       <Heading as="h1">All cabins</Heading>
